24b90460279d0d5581a09a05ed5897a0f039c5d0
Francesco Bottino <francesco.bottino96@gmail.com>
2020-11-20 21:11:00 +0100

    Added collapse behaviour to item list editor view

--- app/src/main/java/it/thefreak/android/interactivecyoaeditor/ui/editor/components/GenericItemsListManager.kt
+++ app/src/main/java/it/thefreak/android/interactivecyoaeditor/ui/editor/components/GenericItemsListManager.kt
@@ -1,6 +1,7 @@
 package it.thefreak.android.interactivecyoaeditor.ui.editor.components
 
 import android.content.Context
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,39 +10,50 @@ import com.mikepenz.fastadapter.drag.ItemTouchCallback
 import com.mikepenz.fastadapter.drag.SimpleDragCallback
 import com.mikepenz.fastadapter.utils.DragDropUtil
 import com.mikepenz.itemanimators.AlphaInAnimator
+import it.thefreak.android.interactivecyoaeditor.hide
 import it.thefreak.android.interactivecyoaeditor.model.ListableItem
 import it.thefreak.android.interactivecyoaeditor.onClick
+import it.thefreak.android.interactivecyoaeditor.show
 import it.thefreak.android.interactivecyoaeditor.ui.editor.binders.ListItemEditorBinder
 import it.thefreak.android.interactivecyoaeditor.ui.editor.binders.ListItemListener
 import it.thefreak.android.interactivecyoaeditor.views.ItemsListEditor
 
-abstract class GenericItemsListManager<T: ListableItem, B: ListItemEditorBinder<T, B>> (
+
+abstract class GenericItemsListManager<T : ListableItem, B : ListItemEditorBinder<T, B>>(
         ctx: Context?,
         itemsListEditor: ItemsListEditor,
-        val binderFactory: (T, ListItemListener<B>)-> B,
+        val binderFactory: (T, ListItemListener<B>) -> B,
         val itemsListActionsListener: ItemsListEditorListener<T>
 ) {
+    private var collapsed: Boolean = false
+
     private val itemsListAdapter: FastItemAdapter<B> = FastItemAdapter()
     private val recyclerView = itemsListEditor.binding.recyclerView
+    private val collapsedViewMore = itemsListEditor.binding.collapsedViewMore
+    private val emptyListPlaceholder = itemsListEditor.binding.emptyListPlaceholder
+
     private val addButton = itemsListEditor.binding.addButton
+    private val collapseButton = itemsListEditor.binding.collapseButton
 
     private val itemsListTouchHelper by lazy {
         ItemTouchHelper(
-            SimpleDragCallback(
-                object : ItemTouchCallback {
-                    override fun itemTouchDropped(oldPosition: Int, newPosition: Int) {
-                        super.itemTouchDropped(oldPosition, newPosition)
-                        updatePointsPositions()
-                    }
-                    override fun itemTouchStartDrag(viewHolder: RecyclerView.ViewHolder) {
-                        super.itemTouchStartDrag(viewHolder)
-                    }
-                    override fun itemTouchOnMove(oldPosition: Int, newPosition: Int): Boolean {
-                        DragDropUtil.onMove(itemsListAdapter.itemAdapter, oldPosition, newPosition)
-                        return true
-                    }
-                }
-            )
+                SimpleDragCallback(
+                        object : ItemTouchCallback {
+                            override fun itemTouchDropped(oldPosition: Int, newPosition: Int) {
+                                super.itemTouchDropped(oldPosition, newPosition)
+                                updatePointsPositions()
+                            }
+
+                            override fun itemTouchStartDrag(viewHolder: RecyclerView.ViewHolder) {
+                                super.itemTouchStartDrag(viewHolder)
+                            }
+
+                            override fun itemTouchOnMove(oldPosition: Int, newPosition: Int): Boolean {
+                                DragDropUtil.onMove(itemsListAdapter.itemAdapter, oldPosition, newPosition)
+                                return true
+                            }
+                        }
+                )
         )
     }
 
@@ -52,6 +64,7 @@ abstract class GenericItemsListManager<T: ListableItem, B: ListItemEditorBinder<
                     remove(getAdapterPosition(item))
                 }
                 updatePointsPositions()
+                updateVisibleContent()
             }
         }
 
@@ -63,8 +76,9 @@ abstract class GenericItemsListManager<T: ListableItem, B: ListItemEditorBinder<
             itemsListActionsListener.onItemCopy(item.content)?.let { copiedItem ->
                 makeBinder(copiedItem).let { copiedItemBinder ->
                     itemsListAdapter.apply {
-                        add(getAdapterPosition(item)+1, copiedItemBinder)
+                        add(getAdapterPosition(item) + 1, copiedItemBinder)
                         updatePointsPositions()
+                        updateVisibleContent()
                     }
                 }
             }
@@ -77,26 +91,66 @@ abstract class GenericItemsListManager<T: ListableItem, B: ListItemEditorBinder<
         }
     }
 
+    private fun updateVisibleContent() {
+        val isEmpty = (itemsListAdapter.itemCount <= 0)
+        if(collapsed) {
+            recyclerView.hide()
+            emptyListPlaceholder.hide()
+            collapsedViewMore.show()
+        } else {
+            if(isEmpty) {
+                emptyListPlaceholder.show()
+                recyclerView.hide()
+            } else {
+                recyclerView.show()
+                emptyListPlaceholder.hide()
+            }
+            collapsedViewMore.hide()
+        }
+    }
+
+    private fun collapse() {
+        if(collapsed) {
+            collapseButton.animate().rotation(0F).interpolator = AccelerateDecelerateInterpolator()
+        } else {
+            collapseButton.animate().rotation(-90F).interpolator = AccelerateDecelerateInterpolator()
+        }
+        collapsed = !collapsed
+        updateVisibleContent()
+    }
+
     init {
         recyclerView.layoutManager = LinearLayoutManager(ctx)
         recyclerView.itemAnimator = AlphaInAnimator()
         itemsListTouchHelper.attachToRecyclerView(recyclerView)
         recyclerView.adapter = itemsListAdapter
 
+        updateVisibleContent()
+
         addButton.onClick {
-            itemsListActionsListener.onNewItem()?.let { newPoints ->
-                newPoints.ordinal = itemsListAdapter.itemCount + 1
-                itemsListAdapter.add(makeBinder(newPoints))
+            itemsListActionsListener.onNewItem()?.let { newItem ->
+                newItem.ordinal = itemsListAdapter.itemCount + 1
+                itemsListAdapter.add(makeBinder(newItem))
+
+                updateVisibleContent()
             }
+        }
+
+        collapseButton.onClick {
+            collapse()
+        }
+        collapsedViewMore.onClick {
+            collapse()
         }
     }
 
     fun set(list: List<T>) {
         itemsListAdapter.set(
-            list
-                .sortedBy { item -> item.ordinal }
-                .map{ makeBinder(it) }
+                list
+                        .sortedBy { item -> item.ordinal }
+                        .map { makeBinder(it) }
         )
+        updateVisibleContent()
     }
 
     private fun makeBinder(item: T): B {
